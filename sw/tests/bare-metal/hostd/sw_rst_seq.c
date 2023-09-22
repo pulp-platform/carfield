3b19a35a290660b4782c00c77f45d0bdf8e9999b
bluew <bluewww@users.noreply.github.com>
2023-09-23 01:13:28 +0200

    sw/tests: Do not try to software reset security domain
    The problem is that this doesn't by work in secure boot mode by
    definition. We don't really have a programmatic way to determine whether
    we are secure booting atm so we need to disable this part of the test.

--- sw/tests/bare-metal/hostd/sw_rst_seq.c
+++ sw/tests/bare-metal/hostd/sw_rst_seq.c
@@ -76,8 +76,9 @@ int main(void)
     car_reset_domain(CAR_L2_RST);
 
     // Security Island
-    // We can't access anything so this needs to be checked manually
-    car_reset_domain(CAR_SECURITY_RST);
+    // We can't access anything so this needs to be checked manually In secure boot mode, the
+    // security island can't be software resetted, which is why we disable this for now
+    // car_reset_domain(CAR_SECURITY_RST);
 
     return 0;
 }
