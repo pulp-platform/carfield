c2f1f9d4fbe7b895da1a03683e352c6778a15bff
bluew <bluewww@users.noreply.github.com>
2023-10-25 15:40:49 +0200

    sw: Add minimal test

---
+++ sw/tests/bare-metal/hostd/minimal.c
@@ -0,0 +1,12 @@
+// Copyright 2023 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Robert Balas <balasr@iis.ee.ethz.ch>
+
+// Minimal test, mainly useful to see whether the return value gets properly read out
+
+int main(void)
+{
+    return 0;
+}
