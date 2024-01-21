d3dbff809e1a21c8114f60d36d949eb4c43cdb5e
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-01-21 23:00:18 +0100

    Bump non-free to extend elab-xilinx-base timeout.

--- carfield.mk
+++ carfield.mk
@@ -44,7 +44,7 @@ include $(CAR_ROOT)/bender-safed.mk
 ######################
 
 CAR_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:carfield/carfield-nonfree.git
-CAR_NONFREE_COMMIT ?= acda49f
+CAR_NONFREE_COMMIT ?= 83c9cd8
 
 ## @section Carfield platform nonfree components
 ## Clone the non-free verification IP for Carfield. Some components such as CI scripts and ASIC
