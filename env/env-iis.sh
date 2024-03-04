37a2220833a40831c426f7a359d2c8b63207bfc6
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-03-04 18:43:03 +0100

    Bump dependencies: * Remove deprecated IPs. * Enforce using `https` for clonig.

--- env/env-iis.sh
+++ env/env-iis.sh
@@ -8,4 +8,5 @@ ROOTD=$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")/.." && pwd)
 export PATH=/usr/pack/riscv-1.0-kgf/riscv64-gcc-11.2.0/bin:$PATH # RV64 GCC toolchain
 export SAFED_RISCV=/usr/pack/riscv-1.0-kgf/pulp-gcc-2.6.0/bin/riscv32-unknown-elf
 export PULPD_RISCV=/usr/pack/pulpsdk-1.0-kgf/artifactory/pulp-sdk-release/pkg/pulp_riscv_gcc/1.0.16/bin/riscv32-unknown-elf
+export SPATZD_RISCV=/usr/pack/riscv-1.0-kgf/riscv64-gcc-9.5.0/bin/riscv64-unknown-elf
 export QUESTA=questa-2023.4
