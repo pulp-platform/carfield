86153e67e9a677540ef42a4ff933213e112f988e
Cyril Koenig <cyril27koenig@gmail.com>
2024-04-24 11:57:40 +0200

    fpga: Testing hyperram in vanilla

--- target/xilinx/flavor_vanilla/scripts/run.tcl
+++ target/xilinx/flavor_vanilla/scripts/run.tcl
@@ -15,7 +15,16 @@ set_param general.maxThreads 8
 # Contraints files selection
 switch $::env(XILINX_BOARD) {
   "vcu128" {
+    # Board specific
     import_files -fileset constrs_1 -norecurse constraints/$::env(XILINX_BOARD).xdc
+    if {[info exists ::env(GEN_EXT_JTAG)] && ($::env(GEN_EXT_JTAG)==1)} {
+      import_files -fileset constrs_1 -norecurse constraints/$::env(XILINX_BOARD)_ext_jtag.xdc
+    }
+    if {![info exists ::env(GEN_NO_HYPERBUS)] || ($::env(GEN_NO_HYPERBUS)==0)} {
+      import_files -fileset constrs_1 -norecurse constraints/$::env(XILINX_BOARD)_hyperbus.xdc
+    }
+
+    # Vanilla specific
     import_files -fileset constrs_1 -norecurse constraints/carfield_top_xilinx.xdc
     # General constraints
     import_files -fileset constrs_1 -norecurse ../constraints/carfield_islands.tcl
