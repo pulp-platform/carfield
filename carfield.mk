5866de4793265a500437b8e0836be6b04c7ae011
Maicol Ciani <81198882+maicolciani@users.noreply.github.com>
2024-05-30 20:24:12 +0200

    [FIX] Aligning L2 size within Linker script (#15)
    * Aligning linker script for L2 with correct memory size
    
    * Bumping CI with L2 hello world

--- carfield.mk
+++ carfield.mk
@@ -47,7 +47,7 @@ include $(CAR_ROOT)/bender-safed.mk
 ######################
 
 CAR_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:astral/astral-nonfree.git
-CAR_NONFREE_COMMIT ?= 6c6b2064e03cccf54c70cb9d754ec112a43332b1 # branch: master
+CAR_NONFREE_COMMIT ?= e8e4354d084cf1cfe6eb9b3bae6af381ef9108b1 # branch: master
 
 ## @section Carfield platform nonfree components
 ## Clone the non-free verification IP for Carfield. Some components such as CI scripts and ASIC
