e763400458bb90499e79c1f09480132f4fa02b12
Yvan Tortorella <ytortorella@lagrev3.ee.ethz.ch>
2024-06-30 07:22:32 +0200

    Bump nonfree.

--- carfield.mk
+++ carfield.mk
@@ -47,7 +47,7 @@ include $(CAR_ROOT)/bender-safed.mk
 ######################
 
 CAR_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:astral/astral-nonfree.git
-CAR_NONFREE_COMMIT ?= 14fc6940f523d94170b49638bd0a8b8c293105b6 # branch: master
+CAR_NONFREE_COMMIT ?= 6c1cd1a67c8fd49db4685e20040030f4a32063a0 # branch: master
 
 ## @section Carfield platform nonfree components
 ## Clone the non-free verification IP for Carfield. Some components such as CI scripts and ASIC
