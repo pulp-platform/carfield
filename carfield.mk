dadb68b1ce3e32a56bb83fbd6e928fb7bc2839f4
Cyril Koenig <cyril27koenig@gmail.com>
2024-07-09 18:37:07 +0200

    vcu118: Ethernet debug

--- carfield.mk
+++ carfield.mk
@@ -44,7 +44,7 @@ include $(CAR_ROOT)/bender-safed.mk
 ######################
 
 CAR_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:carfield/carfield-nonfree.git
-CAR_NONFREE_COMMIT ?= d24785a0
+CAR_NONFREE_COMMIT ?= 5a024891
 
 ## @section Carfield platform nonfree components
 ## Clone the non-free verification IP for Carfield. Some components such as CI scripts and ASIC
