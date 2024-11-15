7df6ba61fdf883bc3c1590bf49f9a559620fe1ab
Cyril Koenig <cyril27koenig@gmail.com>
2024-11-15 16:08:41 +0100

    fpga: Debug once again spi

--- target/xilinx/scripts/flash_spi.tcl
+++ target/xilinx/scripts/flash_spi.tcl
@@ -13,34 +13,27 @@ open_hw_target $::env(XILINX_HOST):$::env(XILINX_PORT)/$::env(XILINX_FPGA_PATH)
 set file $::env(FILE)
 set offset $::env(OFFSET)
 set mcs_file image.mcs
-set mcs_files_x8 [list image_primary.mcs image_secondary.mcs]
 
 if {$::env(XILINX_BOARD) eq "vcu118"} {
   set hw_device [get_hw_devices xcvu9p_0]
-  set hw_mem_device [lindex [get_cfgmem_parts {mt25qu01g-spi-x1_x2_x4_x8}] 0]
-  write_cfgmem -force -format mcs -size 256 -interface SPIx8 \
-    -loaddata "up $offset $file" \
-    -checksum \
-    -file $mcs_file
-  # Two files are generated for spix8
-  set $mcs_file $mcs_files_x8
+  set hw_mem_device [lindex [get_cfgmem_parts {mt25qu01g-spi-x1_x2_x4}] 0]
 }
 
 if {$::env(XILINX_BOARD) eq "vcu128"} {
   set hw_device [get_hw_devices xcvu37p_0]
   set hw_mem_device [lindex [get_cfgmem_parts {mt25qu02g-spi-x1_x2_x4}] 0]
-  write_cfgmem -force -format mcs -size 256 -interface SPIx4 \
-    -loaddata "up $offset $file" \
-    -checksum \
-    -file $mcs_file
 }
 
+write_cfgmem -force -format mcs -size 256 -interface SPIx4 \
+-loaddata "up $offset $file" \
+-checksum \
+-file $mcs_file
 
 set_property PARAM.FREQUENCY 15000000 [get_hw_targets *]
 
 create_hw_cfgmem -hw_device $hw_device $hw_mem_device
 set hw_cfgmem [get_property PROGRAM.HW_CFGMEM $hw_device]
-set_property PROGRAM.FILES $mcs_file $hw_cfgmem
+set_property PROGRAM.FILES [list $mcs_file ] $hw_cfgmem
 set_property PROGRAM.ADDRESS_RANGE  {use_file} $hw_cfgmem
 set_property PROGRAM.PRM_FILE {} $hw_cfgmem
 set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} $hw_cfgmem
