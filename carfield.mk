8c927e858c38594e1ea1609f0391ed31d1fbb911
aottaviano <aottaviano@iis.ee.ethz.ch>
2025-05-12 17:00:17 +0200

    carfield.mk: Bump nonfree dependency
    * Add spatz offload test from host as a regression to CI

--- carfield.mk
+++ carfield.mk
@@ -57,7 +57,7 @@ include $(CAR_ROOT)/bender-safed.mk
 ######################
 
 CAR_NONFREE_REMOTE ?= git@iis-git.ee.ethz.ch:carfield/carfield-nonfree.git
-CAR_NONFREE_COMMIT ?= c2c864ca
+CAR_NONFREE_COMMIT ?= f21a428
 
 ## @section Carfield platform nonfree components
 ## Clone the non-free verification IP for Carfield. Some components such as CI scripts and ASIC
