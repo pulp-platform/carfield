edaf0150845e5ea9cc159c40f6d0b5e9de3fe11c
Cyril Koenig <cyril27koenig@gmail.com>
2026-06-11 18:04:37 +0200

    HeroCI: Make second L2 port uncached

--- sw/include/car_params.h
+++ sw/include/car_params.h
@@ -16,8 +16,8 @@ const void* car_integer_cluster = 0x50000000;
 const void* car_spatz_cluster = 0x51000000;
 const void* car_l2_intl_0 = 0x78000000;
 const void* car_l2_cont_0 = 0x78100000;
-const void* car_l2_intl_1 = 0x78200000;
-const void* car_l2_cont_1 = 0x78300000;
+const void* car_l2_intl_1 = 0x68000000;
+const void* car_l2_cont_1 = 0x68100000;
 
 #else // Pointers to be mapped by the driver
 void* car_soc_ctrl;
